9a915298eb46b539272925a5c41f0f1098743853
coghcjf2@gmail.com <coghcjf2@gmail.com>
2022-01-15 11:20:04 +0900

    first commit

---
+++ .vscode/randint.py
@@ -0,0 +1,39 @@
+#from random import randint
+
+#for _ in range(4):
+#    print(randint(0,255))
+
+# 리스트 컴프리핸션으로 변경하는 방법
+# for문을 한 줄로 줄인다.
+# 콜론(:)을 삭제한다.
+# 실행문을 왼쪽으로 이동시킨다.
+# print()를 삭제한다.
+# 대괄호([]) 를 붙인다.
+# ip = 변수에 할당한다.
+#for _ in range(4):
+#    print(randint(0,255))
+#ip = [randint(0,255) for _ in range(4)]
+#print(ip, type(ip[0]))
+
+# 7. str()
+# 정수를 인수로 넘겨주면 문자열로 리턴한다.
+# >>> a = 100
+# >>> a
+# 100
+# >>> type(a)
+# <class 'int'>
+# >>> a = str(a)
+# >>> a
+# '100'
+# >>> type(a)
+# <class 'str'>
+
+#>>> a= 100
+#>>> type(a)
+#<class 'int'>
+#>>> a = str(a)
+#>>> a
+#'100'
+#>>> type(a)    
+#<class 'str'>
+
